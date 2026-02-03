9207abfb2ba97410e00274e384345520c31fdf29
ATG17 <bhakti17204@gmail.com>
2026-02-03 10:14:25 +0530

    2x1 mux
    Concurrent VHDL example

--- Concurrent_Statements/when_else/mux_when_else.vhd
+++ Concurrent_Statements/when_else/mux_when_else.vhd
@@ -1,1 +1,17 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity mux_2x1 is
+    Port (
+        a   : in  STD_LOGIC;
+        b   : in  STD_LOGIC;
+        sel : in  STD_LOGIC;
+        y   : out STD_LOGIC
+    );
+end mux_2x1;
+
+architecture concurrent of mux_2x1 is
+begin
+    y <= a when sel = '0' else b;
+end concurrent;
 
