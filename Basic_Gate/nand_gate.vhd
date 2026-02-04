a036bdb806625df90f65f943a98e0a9ce872a75e
Bhakti <bhakti17204@gmail.com>
2026-02-04 14:12:43 +0530

    Add NAND gate implementation in VHDL

---
+++ Basic_Gate/nand_gate.vhd
@@ -0,0 +1,15 @@
+library IEEE;
+use IEEE.STD_LOGIC_1164.ALL;
+
+entity nand_gate is
+    Port (
+        a : in  STD_LOGIC;
+        b : in  STD_LOGIC;
+        y : out STD_LOGIC
+    );
+end nand_gate;
+
+architecture concurrent of nand_gate is
+begin
+    y <= not (a and b);
+end concurrent;
